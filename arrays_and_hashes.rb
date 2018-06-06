814823201cc7895655a54ed655308372ad05054a
hibbahagh <hibbahagh@gmail.com>
2018-06-06 21:52:34 +0000

    automatically backed up by learn

---
+++ arrays_and_hashes.rb
@@ -0,0 +1,11 @@
+closet ={ 
+  "tops" => [ "rain jacket", "cardigon", "blazer"]
+  "bottoms"=> [ "jeans","skirt","capris"]
+  "footwear"=> {
+    "sport"=> ["sneakers", "rock climbing", "cleats"]
+      "casual"=> ["flats","boots"]
+      "formal"=> [ "heels","pumps"]
+  }
+}
+
+ puts closet 
--- index.rb
+++ index.rb
@@ -1,39 +1,53 @@
 # example ingredients array
-ingredients = [
-    ["rye", "sourdough", "baguette"],
-    ["ham", "salami", "turkey"],
-    ["swiss", "munster", "cheddar"],
-    ["mayo", "mustard", "tabasco"]
+# ingredients = [
+#     ["rye", "sourdough", "baguette"],
+#     ["ham", "salami", "turkey"],
+#     ["swiss", "munster", "cheddar"],
+#     ["mayo", "mustard", "tabasco"]
 ]
+# if the order found within the ingredients then it's true
+# otherwise it's false 
 
 def sandwich_request(ingredients, order) 
-  # your code here!
+  ingredients = [
+   ["rye", "sourdough", "baguette"],
+   ["ham", "salami", "turkey"],
+   ["swiss", "munster", "cheddar"],
+   ["mayo", "mustard", "tabasco"]
+]
+
+if ingredients == order 
+  puts "true"
+else 
+
+  puts "false"
+end 
 end
 
 # example city_info hash
-cities = {
-  :new_york => { 
-    :mayor => "Bill DeBlasio",
-    :population => 8337000,
-    :website => "http://www.nyc.gov"
-  },
-  :los_angeles => { 
-    :mayor => "Eric Garcetti",
-    :population => 3884307,
-    :website => "http://www.lacity.org"
-  },
-  :miami => { 
-    :mayor => "Tomás Regalado",
-    :population => 419777,
-    :website => "http://www.miamigov.com"
-  },
-  :chicago => { 
-    :mayor => "Rahm Emanuel",
-    :population => 2695598,
-    :website => "http://www.cityofchicago.org/"
-  }
-}
+# cities = {
+#   :new_york => { 
+#     :mayor => "Bill DeBlasio",
+#     :population => 8337000,
+#     :website => "http://www.nyc.gov"
+#   },
+#   :los_angeles => { 
+#     :mayor => "Eric Garcetti",
+#     :population => 3884307,
+#     :website => "http://www.lacity.org"
+#   },
+#   :miami => { 
+#     :mayor => "Tomás Regalado",
+#     :population => 419777,
+#     :website => "http://www.miamigov.com"
+#   },
+#   :chicago => { 
+#     :mayor => "Rahm Emanuel",
+#     :population => 2695598,
+#     :website => "http://www.cityofchicago.org/"
+#   }
+# }
 
-def city_populations(cities)
+# def city_populations(cities)
   # your code here!
-end
+# end
