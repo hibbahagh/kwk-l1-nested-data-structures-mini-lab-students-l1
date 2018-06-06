25ec22c29c2ba1df4191311c6fb6c275a30a7c1a
hibbahagh <hibbahagh@gmail.com>
2018-06-06 21:54:24 +0000

    automatically backed up by learn

--- index.rb
+++ index.rb
@@ -9,7 +9,11 @@ ingredients = [
 # otherwise it's false 
 
 def sandwich_request(ingredients, order) 
-  
+  ingredients = [
+    ["rye", "sourdough", "baguette"],
+    ["ham", "salami", "turkey"],
+    ["swiss", "munster", "cheddar"],
+    ["mayo", "mustard", "tabasco"]
 
 if ingredients == order 
   puts "true"
